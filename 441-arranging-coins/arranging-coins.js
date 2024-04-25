3c66bd0bd2527a4efb3ab44aa921c0a8b7671590
Ashish john <135585579+Splashish@users.noreply.github.com>
2024-04-25 21:33:41 +0530

    Time: 92 ms (37.08%) | Memory: 52.9 MB (82.08%) - LeetSync

---
+++ 441-arranging-coins/arranging-coins.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var arrangeCoins = function (n) {
+    let sum = 0;
+    for(let i=1;i<=n;i++){
+        sum = sum + i
+        if(sum === n){
+            return i
+        }
+        if(sum >= n){
+            return i-1
+        }
+    }
+};
