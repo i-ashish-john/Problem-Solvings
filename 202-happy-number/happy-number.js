ed9b9ef3f80f26de65f98b55d60ffb4910356589
Ashish john <135585579+Splashish@users.noreply.github.com>
2024-05-11 23:05:46 +0530

    Time: 55 ms (72.59%) | Memory: 49.2 MB (87.87%) - LeetSync

--- 202-happy-number/happy-number.js
+++ 202-happy-number/happy-number.js
@@ -1,21 +1,18 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
 var isHappy = function(n) {
     let number = n.toString()
-    
     let seen = new Set();
-    
     while(!seen.has(number)){
         let result = 0;
-
         for(let i = 0; i < number.length; i++){
             result += Math.pow(parseInt(number[i]), 2)
         }
-
         if(result == 1) return true
-            
         seen.add(number)
-        
         number = result.toString();
     }
-
     return false;
 };
