67a74ad3e061b3ba352ae6a0286c94e5db4673e6
Ashish john <135585579+Splashish@users.noreply.github.com>
2024-05-24 12:21:55 +0530

    Time: 63 ms (14.95%) | Memory: 48.6 MB (86.90%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.js
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.js
@@ -4,17 +4,17 @@
  */
 var sumOfUnique = function(nums) {
   let sum =0
-  let nums1 = [...nums]
+//   let nums1 = [...nums]
   for(let i=0; i<nums.length;i++){
     // let sorted =nums1.sort((a,b)=>b-a)
     // console.log('sorted',sorted);
-    let flag =0 
+    let flag = true;
     for(let j=0;j< nums.length;j++){
         if(nums[j]===nums[i] &&  i!=j){
-            flag = 1
+            flag = false
         }
     }
-    if(flag==0){
+    if(flag==true){
         sum= sum+nums[i]
     }
     
