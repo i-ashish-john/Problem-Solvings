f0fe33f1f9dbdcb2670afbabb1e359a0a0859531
Ashish john <135585579+Splashish@users.noreply.github.com>
2024-06-01 23:42:21 +0530

    Time: 60 ms (85.59%) | Memory: 52.1 MB (48.59%) - LeetSync

---
+++ 941-sort-array-by-parity/sort-array-by-parity.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity =(nums)=>{
+    let j=0;
+    for(let i = 0;i<nums.length;i++){
+        if(nums[i]%2==0){
+            let temp=nums[j];
+            nums[j]=nums[i];
+             nums[i]=temp;
+            j++;
+        }
+    }
+    return nums;
+};
