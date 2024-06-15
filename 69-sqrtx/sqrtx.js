b29022f48d76c6da51aae556a260debd6029f00d
Ashish john <135585579+Splashish@users.noreply.github.com>
2024-06-15 23:22:03 +0530

    Time: 73 ms (67.70%) | Memory: 52.9 MB (30.86%) - LeetSync

---
+++ 69-sqrtx/sqrtx.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+   let root = Math.sqrt(x) 
+   let val = Math.floor(root)
+   return val
+//    console.log('sqrt of root is =',root);
+};
