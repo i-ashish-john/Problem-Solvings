40dae446dbf4522108b45f928ca99b145ff0d0b7
Ashish john <135585579+Splashish@users.noreply.github.com>
2024-07-01 23:46:09 +0530

    Time: 48 ms (77.55%) | Memory: 48.9 MB (52.04%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var threeConsecutiveOdds = function(arr) {
+    for (let i = 2; i < arr.length; i++) {
+        if (arr[i] & 1 && arr[i - 1] & 1 && arr[i - 2] & 1) {
+            return true;
+        }
+    }
+
+    return false;
+};
