6f6fcca4e225ede2ddd4561da695f0ce65ac474a
Ashish john <135585579+Splashish@users.noreply.github.com>
2024-07-15 23:43:00 +0530

    Time: 66 ms (24.67%) | Memory: 50.9 MB (49.16%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+    s = s.split("").sort();
+    t = t.split("").sort();
+    for(let i=0;i<s.length;i++){
+        if(s[i]!==t[i]) return t[i];
+    }
+    return t[t.length-1];
+};
