fba4502b952f036d6f162dd325ddb24575011bb7
Ashish john <135585579+i-ashish-john@users.noreply.github.com>
2024-09-03 00:16:17 +0530

    Time: 71 ms (85.54%) | Memory: 62.2 MB (12.05%) - LeetSync

---
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} chalk
+ * @param {number} k
+ * @return {number}
+ */
+var chalkReplacer = function(chalk, k) {
+    let sum=0
+    for(let x of chalk)sum+=x
+    let rem=k%sum
+    for(let i=0;i<chalk.length;i++){
+        rem-=chalk[i]
+        if(rem<0)return i
+    }
+};
