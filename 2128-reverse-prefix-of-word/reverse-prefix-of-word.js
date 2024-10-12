9b834943111bb00cdfabc69901b272c3eb63a28c
Ashish john <135585579+i-ashish-john@users.noreply.github.com>
2024-10-12 23:58:24 +0530

    Time: 39 ms (97.39%) | Memory: 48.8 MB (76.53%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.js
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.js
@@ -6,11 +6,9 @@
 var reversePrefix = function(word, ch) {
     const index = word.indexOf(ch);
     
-    // If ch is not found, return the original word
     if (index === -1) {
         return word;
     }
     
-    // Reverse the prefix and concatenate with the rest of the word
-    return word.slice(0, index + 1).split('').reverse().join('') + word.slice(index + 1);
+    return word.slice(0,index + 1).split('').reverse().join('')+word.slice(index+1);
 };
