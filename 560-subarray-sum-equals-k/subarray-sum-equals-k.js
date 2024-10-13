6e8893b2998cac041ffab42d8ab93a057e7544e7
Ashish john <135585579+i-ashish-john@users.noreply.github.com>
2024-10-13 23:52:44 +0530

    Time: 1700 ms (5.05%) | Memory: 51.1 MB (99.53%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+// var subarraySum = function(nums, k) {
+//     let count=0 
+//     for(let i = 0;i<nums.length;i++){
+//         if(nums[i]==k || nums[i]+nums[i+1]==k ){
+//             count++
+//         }
+//     }
+//     return count
+// };
+var subarraySum = function(nums, k) {
+    let counter = 0;
+    for (let i=0;i<nums.length;i++) {
+        let base = 0;
+        for (let j=i;j<nums.length;j++) {
+            base+=nums[j];
+            if (base == k) counter++;
+        }
+    }
+    return counter;
+};
