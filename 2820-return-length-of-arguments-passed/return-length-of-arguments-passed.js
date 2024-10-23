d72a6ddc8f337b770e7b9fef6228d18859be3067
Ashish john <135585579+i-ashish-john@users.noreply.github.com>
2024-10-23 12:41:20 +0530

    Time: 47 ms (80.51%) | Memory: 48.4 MB (87.78%) - LeetSync

--- 2820-return-length-of-arguments-passed/return-length-of-arguments-passed.js
+++ 2820-return-length-of-arguments-passed/return-length-of-arguments-passed.js
@@ -3,12 +3,15 @@
  * @return {number}
  */
 var argumentsLength = function(...args) {
-    let count =0
-    for(let i = 0;i<args.length;i++){
-        count++
-    }
+    // let count =0
+    // for(let i = 0;i<args.length;i++){
+    //     count++
+    // }
 
-    return count
+    // return count
+
+
+    return args.length
 };
 
 /**
