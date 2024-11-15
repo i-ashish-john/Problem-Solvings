6bf394217f1f8066dc1e2285023d35a176d1d279
Ashish john <135585579+i-ashish-john@users.noreply.github.com>
2024-11-15 23:23:52 +0530

    Time: 7 ms (11.88%) | Memory: 61.4 MB (25.03%) - LeetSync

---
+++ 189-rotate-array/rotate-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {    
+    let n = nums.length
+    let numsCopy = [...nums];
+    console.log(numsCopy)
+    for (let i = 0; i < n; i++) {
+	nums[(i+k)%n] = numsCopy[i]
+    }    
+};
