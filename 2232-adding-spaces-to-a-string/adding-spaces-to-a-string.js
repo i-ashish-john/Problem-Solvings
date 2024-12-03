f878255abdd0b8d989df7a2c40c64c6eec53ce55
Ashish john <135585579+i-ashish-john@users.noreply.github.com>
2024-12-03 11:32:47 +0530

    Time: 84 ms (74.55%) | Memory: 101 MB (11.29%) - LeetSync

--- 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.js
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.js
@@ -8,10 +8,10 @@ var addSpaces = function(s, spaces) {
      let result = []
      let count  =0
      for(let i =0;i<str.length;i++){
-     if (count < spaces.length && spaces[count] === i) {
-        result.push(' ');  
-        count++;  
-     }
+        if (count < spaces.length && spaces[count] === i) {
+            result.push(' ');  
+            count++;  
+        }
     result.push(str[i]); 
      }
      return result.join('')
